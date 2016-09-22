efc2094718d83207d19447798280f098dc6910cf
jundl77 <julianbrendl@gmail.com>
2016-09-22 15:34:25 +0200

    added google lint style guide (omg this thing is strict, it even wants newlines at the end of each file :O)

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 
@@ -13,7 +13,7 @@ app.get('*', (req, res) => {
 // Express server
 const port = process.env.PORT || 8080;
 const server = app.listen(port, () => {
-    const host = server.address().address;
-    const port = server.address().port;
-    console.log('Essential React listening at http://%s:%s', host, port);
+  const host = server.address().address;
+  const port = server.address().port;
+  console.log('Essential React listening at http://%s:%s', host, port);
 });
